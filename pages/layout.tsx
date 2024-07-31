a9dd2fbebf06c33e9f69b9cad8914e536484f194
Darzhan <eduardovdarzhan@gmail.com>
2024-07-31 13:55:35 +0500

    fixing deployment issues

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,13 +1,18 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Poppins } from "next/font/google";
+import { ReactNode } from "react";
 
 const poppins = Poppins({
 	subsets: ["latin"],
 	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-export default function Layout({ children }) {
+interface LayoutProps {
+	children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
 	return (
 		<main className={`min-h-screen overflow-x-hidden ${poppins.className}`}>
 			<Navbar />
